4422cb23a9805d169e08a14396b92da7f87ca3d5
Ahmad Aghazadeh <890683@DBI.COM>
2022-01-20 08:11:25 +0330

    Inline Function

--- kotlin/src/main/java/com/example/kotlin/ClassSample.kt
+++ kotlin/src/main/java/com/example/kotlin/ClassSample.kt
@@ -13,14 +13,22 @@ class Truck(color: String, model: String) : Car(color, model){
 }
 
 open class Car(val color:String, var model:String){
-
     init {
         //color="Green"
         model="Benz "
     }
-
     open fun drive(){
         println("Drive vroom")
     }
+}
+
+class Button(val lable:String):ClickEvent{
+    override fun onClick(message: String) {
+        TODO("Not yet implemented")
+    }
+}
+
+interface ClickEvent{
+    fun onClick(message:String)
 }
 
